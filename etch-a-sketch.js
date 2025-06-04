ea905b8fa00e5a4404873d7d90ebc29a6977d215
Angela <angdkay@gmail.com>
2025-06-03 18:17:44 -0600

    Add ability to change colour of grid
    Ensure proper colouring when clicking and dragging
    through the canvas.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,49 @@
-divContainer = document.querySelector(".grid-container");
-
+// Generates the square
+const divContainer = document.querySelector(".grid-container");
+const containerSize = divContainer.offsetWidth;
 divs = [];
 
-for (i = 1; i <= 16; i++) {
-  for (j = 1; j <= 16; j++) {
-    console.log("hey");
+let squareSize = 16;
+let clicked = false;
+
+for (i = 1; i <= squareSize; i++) {
+  for (j = 1; j <= squareSize; j++) {
     const div = document.createElement("div");
-    div.classList.add(`div${i}`);
-    div.style.width = "20px";
-    div.style.height = "20px";
+    div.style.width = containerSize / squareSize + "px";
+    div.style.height = containerSize / squareSize + "px";
     div.style.backgroundColor = "brown";
+    divs.push(div);
     divContainer.appendChild(div);
   }
 };
+
+
+divs.forEach(div => {
+
+  // keeps track if user wants to colour multiple divs
+  div.addEventListener('mousedown', () => {
+    clicked = true;
+  })
+  div.addEventListener('mouseup', () => {
+    clicked = false;
+  })
+
+  // lets user color one div
+  div.addEventListener('click', () => {
+    changeDivColor(div); 
+  });
+
+  // if user is pressing down and dragging (via clicked = true), colours divs
+  div.addEventListener('mousemove', () => {
+    if (clicked) {
+      changeDivColor(div);
+    }
+  });
+});
+
+function changeDivColor(div) {
+  /**
+   * Changes the Color of the Div
+   */
+  div.style.backgroundColor = "blue";
+}
